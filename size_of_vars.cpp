99bd944409e52ab8b4e6c1fbfc0e3e9310171095
Shweta goel <31646342+goelShweta@users.noreply.github.com>
2018-03-10 11:24:12 +0530

    Add files via upload

---
+++ size_of_vars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //string a[] ="abcdefgh";
+    cout <<"integer size "<<sizeof(int)<<endl;
+    cout <<"character size "<<sizeof(char)<<endl;
+    cout <<"float size "<<sizeof(float)<<endl;
+    cout <<"double size "<<sizeof(double)<<endl;
+    //cout <<"string size "<<sizeof(string)<<endl;
+    //cout <<"long size "<<sizeof(long)<<endl;
+    //cout <<"short size "<<sizeof(short)<<endl;
+    cout <<"long int size "<<sizeof(long int)<<endl;
+    cout <<"unsigned long int size "<<sizeof(unsigned long int)<<endl;
+    cout <<"signed long int size "<<sizeof(signed long int)<<endl;
+    return 0;
+}
