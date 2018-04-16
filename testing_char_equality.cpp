2674fb9dd040e89ba5a13cb508f69a05f0137b0b
Shweta goel <31646342+goelShweta@users.noreply.github.com>
2018-04-16 06:38:09 +0530

    Add files via upload

---
+++ testing_char_equality.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char a='abx',b='cgd',c='abx';
+	if(a==c)
+	 cout<<"equal";
+	else
+	cout<<"not equal"; 
+	
+		if(b<a)
+	 cout<<"less";
+	else
+	cout<<"not less"; 
+	
+	if(c<a)
+	 cout<<"less";
+	else
+	cout<<"not less"; 
+	return 0;
+}
